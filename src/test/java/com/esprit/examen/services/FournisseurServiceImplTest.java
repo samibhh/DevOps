3745f76ea6977c3a0169dbfd8b3cb319aa1d4a02
samibhh <58134231+samibhh@users.noreply.github.com>
2022-11-12 18:01:40 +0100

    Update FournisseurServiceImplTest.java

--- src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -13,20 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.CategorieFournisseur;
 import com.esprit.examen.entities.DetailFournisseur;
 import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.entities.SecteurActivite;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FournisseurServiceImplTest {
 	@Autowired
 	IFournisseurService fs;
-	/*
+	@Autowired
+	ISecteurActiviteService sas;
+	
 	private Fournisseur initF()
 	{
 	    Date date = new Date(0);  
 		DetailFournisseur detailFournisseur=new DetailFournisseur(null, "fournisseur@gmail.com",date,"Tunis","RandomMat", null);
 		Fournisseur f = new Fournisseur(null,"RandomCode","FournisseurTest",CategorieFournisseur.ORDINAIRE,null,null,detailFournisseur);
 		return f;
-	}*/
+	}
 	@Test
 	public void testRetrieveAllFournisseurs(){
 		assertNotNull(fs.retrieveAllFournisseurs());
@@ -35,11 +38,8 @@ public class FournisseurServiceImplTest {
 	public void testAddFournisseur() {
 	//	List<Stock> stocks = stockService.retrieveAllStocks();
 	//	int expected=stocks.size();
-		Date date = new Date(0);  
-		DetailFournisseur detailFournisseur=new DetailFournisseur(null, "fournisseur@gmail.com",date,"Tunis","RandomMat", null);
-		Fournisseur f = new Fournisseur(null,"RandomCode","FournisseurTest",CategorieFournisseur.ORDINAIRE,null,null,detailFournisseur);
-		
-		//Fournisseur f=initF();
+
+		Fournisseur f=initF();
 		Fournisseur savedFournisseur= fs.addFournisseur(f);
 		
 	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
@@ -50,16 +50,35 @@ public class FournisseurServiceImplTest {
 	
 	@Test
 	public void testDeleteFournissuer() {
-		//Fournisseur f=initF();
-		Date date = new Date(0);  
-		DetailFournisseur detailFournisseur=new DetailFournisseur(null, "fournisseur@gmail.com",date,"Tunis","RandomMat", null);
-		Fournisseur f = new Fournisseur(null,"RandomCode","FournisseurTest",CategorieFournisseur.ORDINAIRE,null,null,detailFournisseur);
-		
+		Fournisseur f=initF();
+
 		Fournisseur savedFournisseur= fs.addFournisseur(f);
 		fs.deleteFournisseur(savedFournisseur.getIdFournisseur());
 		
 		assertNull(fs.retrieveFournisseur(savedFournisseur.getIdFournisseur()));
 	}
+	@Test
+	public void testupdateFournisseur() {
+		Fournisseur f=initF();
 
-	
+		Fournisseur savedFournisseur= fs.addFournisseur(f);
+		fs.updateFournisseur(savedFournisseur);
+		assertNull(fs.retrieveFournisseur(savedFournisseur.getIdFournisseur()));
+
+	}
+	@Test
+	public void testassignSecteurActiviteToFournisseur(){
+		Fournisseur f=initF();
+		SecteurActivite s=new SecteurActivite((long)909,"TestCode","LibelleCode",null);
+		Fournisseur savedFournisseur= fs.addFournisseur(f);
+		SecteurActivite savedSA=sas.addSecteurActivite(s);
+		fs.assignSecteurActiviteToFournisseur(s.getIdSecteurActivite(),savedFournisseur.getIdFournisseur());
+		assertNull(savedFournisseur.getSecteurActivites().toString());
+		fs.deleteFournisseur(savedFournisseur.getIdFournisseur());
+	sas.deleteSecteurActivite(savedSA.getIdSecteurActivite());
+
+
+		
+	}
+
 }
